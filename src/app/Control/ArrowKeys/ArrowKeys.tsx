374cf64f97f33c2578c67b6c403f521250ec0f02
Nico Martin <mail@nicomartin.ch>
2021-08-02 14:22:31 +0200

    added keyboard events to ArrowKeys interface

--- src/app/Control/ArrowKeys/ArrowKeys.tsx
+++ src/app/Control/ArrowKeys/ArrowKeys.tsx
@@ -36,6 +36,29 @@ const ArrowKeys = ({
 
   const stop = () => onCmdStop();
 
+  const keydown = (e) => {
+    if (e.keyCode == '38') {
+      forward();
+    } else if (e.keyCode == '40') {
+      backward();
+    } else if (e.keyCode == '37') {
+      left();
+    } else if (e.keyCode == '39') {
+      right();
+    }
+  };
+
+  const keyup = () => stop();
+
+  React.useEffect(() => {
+    window.addEventListener('keydown', keydown);
+    window.addEventListener('keyup', keyup);
+    return () => {
+      window.removeEventListener('keydown', keydown);
+      window.removeEventListener('keyup', keyup);
+    };
+  });
+
   return (
     <div className={cn(styles.root, className)}>
       <div className={styles.buttons}>
